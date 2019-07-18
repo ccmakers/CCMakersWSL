3a73693be0ef50b66ac145bb2629e59e587a0a3b
Rodrigo Passos <rodrigo@hellodative.com>
2019-07-17 21:24:46 -0400

    implement blink example

--- examples/CCMakersWSLTest/CCMakersWSLTest.ino
+++ examples/CCMakersWSLTest/CCMakersWSLTest.ino
@@ -3,13 +3,39 @@
 
 CCMakersWSL webserver("MakerBeacon", "MakerNode");
 
-void testHandler() {
-  String webpage = "<h3>testing</h3>";
-  webserver.send(200, "text/html", webserver.createPage(webpage)); //Send web page
+void blinkHandler() {
+  String webpage = R"=====(
+    <h3>Blink Test</h3>
+    <button id="blinkButton">blink</button>
+  )=====";
+
+  String js = R"=====(
+    var element = document.getElementById('blinkButton');
+    element.addEventListener('click', function() {
+      fetch('/do-blink')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+      });
+    });
+  )=====";
+  webserver.send(200, "text/html", webserver.createPage(webpage, js)); //Send web page
+}
+
+void doBlink() {
+  webserver.send(200, "text/plain", "ok"); //Send web page
+  digitalWrite(2, 0);
+  delay(1000);
+  digitalWrite(2, 1);
 }
 
 void setup() {
-  webserver.on("/test", testHandler);
+  digitalWrite(2, 1);
+  pinMode(2, OUTPUT);
+  webserver.on("/blink", blinkHandler);
+  webserver.on("/do-blink", doBlink);
   webserver.begin();
 }
 
