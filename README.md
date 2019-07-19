7925ab9772a628e1a858623b6a82a3df5af4e4d7
Rodrigo Passos <rodrigo@hellodative.com>
2019-07-19 17:59:16 -0400

    update examples and docs

--- README.md
+++ README.md
@@ -1,5 +1,52 @@
 # CCMakersWSL
 
+The CCMakersWSL class is a wrapper for [esp8266](https://github.com/esp8266/) WiFi and Web Server functions. It's meant to be used for teaching purpouses only.
+
 ## Installation
 
 ## Usage
+
+### Class Constructor
+
+```cpp
+CCMakersWSL webserver("ssid", "hostname");
+```
+
+Creates the CCMakersWSL class object.
+
+*Parameters:*
+
+WiFi Name: `String ssid`
+Hostname: `String hostname`
+
+### Simple start
+
+#### Starting the server
+
+```cpp
+#include <CCMakersWSL.h>
+
+// Declare in global scope
+CCMakersWSL webserver("MakerBeacon", "MakerNode");
+
+// define route handler function
+void testHandler() {
+  webserver.send(200, "text/plain", "test route");
+}
+
+void setup() {
+  // Boot up WiFi
+  webserver.bootWiFi();
+
+  // Register your custom routes
+  webserver.on("/test", testHandler);
+
+  // Boot up Web User Interface
+  webserver.bootWebUI();
+}
+
+void loop() {
+  // Handling incoming client requests
+  webserver.handleClient();
+}
+```
--- examples/CCMakersWSLTest/CCMakersWSLTest.ino
+++ examples/CCMakersWSLTest/CCMakersWSLTest.ino
@@ -3,40 +3,17 @@
 
 CCMakersWSL webserver("MakerBeacon", "MakerNode");
 
-void blinkHandler() {
+void testHandler() {
   String webpage = R"=====(
-    <h3>Blink Test</h3>
-    <button id="blinkButton">blink</button>
+    <h3>Test Route</h3>
   )=====";
-
-  String js = R"=====(
-    var element = document.getElementById('blinkButton');
-    element.addEventListener('click', function() {
-      fetch('/do-blink')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(myJson) {
-        console.log(JSON.stringify(myJson));
-      });
-    });
-  )=====";
-  webserver.send(200, "text/html", webserver.createPage(webpage, js)); //Send web page
-}
-
-void doBlink() {
-  webserver.send(200, "text/plain", "ok"); //Send web page
-  digitalWrite(2, 0);
-  delay(1000);
-  digitalWrite(2, 1);
+  webserver.send(200, "text/html", webserver.createPage(webpage));
 }
 
 void setup() {
-  digitalWrite(2, 1);
-  pinMode(2, OUTPUT);
-  webserver.on("/blink", blinkHandler);
-  webserver.on("/do-blink", doBlink);
-  webserver.begin();
+  webserver.bootWiFi();
+  webserver.on("/test", testHandler);
+  webserver.bootWebUI();
 }
 
 void loop() {
